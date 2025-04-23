bf74a91d8a9c6304ba6abbfe8736efc9a1202e7a
André Queiroz de Araújo Viana <andreviana@caesb.df.gov.br>
2025-04-23 15:26:40 -0300

    many to many

--- src/main/java/com/projetowebservice/Projeto/entities/Product.java
+++ src/main/java/com/projetowebservice/Projeto/entities/Product.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Table(name =  "tb_product")
+@Table(name = "tb_product")
 public class Product implements Serializable {
     private static final long serialVersion = 1L;
 
@@ -20,9 +20,13 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Product(){}
+    public Product() {
+    }
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
