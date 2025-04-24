cceb6c2afca2a839fd83588d4feb9c648740dc1f
André Queiroz de Araújo Viana <andreviana@caesb.df.gov.br>
2025-04-24 16:57:05 -0300

    User Insert

--- src/main/java/com/projetowebservice/Projeto/resources/UserResource.java
+++ src/main/java/com/projetowebservice/Projeto/resources/UserResource.java
@@ -2,12 +2,13 @@ package com.projetowebservice.Projeto.resources;
 
 import com.projetowebservice.Projeto.entities.User;
 import com.projetowebservice.Projeto.services.UserService;
+import jakarta.persistence.OneToOne;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,6 +28,14 @@ public class UserResource {
     public  ResponseEntity<User> findById(Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
