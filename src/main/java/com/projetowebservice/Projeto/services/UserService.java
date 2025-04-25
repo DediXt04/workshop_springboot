07528946eae7f8f5d584bacfdfe73f7c49e82085
André Queiroz de Araújo Viana <andreviana@caesb.df.gov.br>
2025-04-25 14:16:07 -0300

    Exception

--- src/main/java/com/projetowebservice/Projeto/services/UserService.java
+++ src/main/java/com/projetowebservice/Projeto/services/UserService.java
@@ -2,6 +2,7 @@ package com.projetowebservice.Projeto.services;
 
 import com.projetowebservice.Projeto.entities.User;
 import com.projetowebservice.Projeto.repository.UserRepository;
+import com.projetowebservice.Projeto.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj= repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
